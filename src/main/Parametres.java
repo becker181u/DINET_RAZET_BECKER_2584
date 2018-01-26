f50397eda55f05ebb9c4fc3a0af5326906f91848
becker181u <becker181u@etu.univ-lorraine.fr>
2018-01-26 10:57:04 +0100

    grille et parametre

--- src/main/Parametres.java
+++ src/main/Parametres.java
@@ -3,5 +3,9 @@ package main;
 public interface Parametres {
 
 	static final int OBJECTIF = 2584;
-	
+    static final int HAUT = 1;
+    static final int DROITE = 2;
+    static final int BAS = -1;
+    static final int GAUCHE = -2;
+    static final int TAILLE = 4;
 }
