e7c06c21d596b3c8d5d75f91d005847d0b0e4bd5
becker181u <becker181u@etu.univ-lorraine.fr>
2018-02-28 09:40:22 +0100

    Ajout package test

---
+++ src/test_javafx/Test.java
@@ -0,0 +1,30 @@
+package test_javafx;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Test extends Application{
+
+	public static void main(String[] args) {
+		Application.launch(Test.class, args);
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		Group root = new Group();
+		Scene scene = new Scene(root, 1000,600,Color.LIGHTBLUE);
+		primaryStage.setScene(scene);
+		
+		primaryStage.set
+		
+	}
+
+}
