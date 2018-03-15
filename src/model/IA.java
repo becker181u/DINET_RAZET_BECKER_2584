50f8bbc37769dcf07eb49792eac478d0f8558e31
guilherme razet <guilherme.razet@gmail.com>
2018-03-15 15:16:34 +0100

    IA #1

--- src/model/IA.java
+++ src/model/IA.java
@@ -2,17 +2,54 @@ package model;
 
 public class IA extends Joueur implements Parametres{
 	
+	private int scoreSec;
+	private Grille grilleSec;
+
 	public IA(){
 		super();
-		Grille grilleSec = grille;
+	}
+	
+	public int getScoreSec(){
+		return this.scoreSec;
+	}
+	
+	public void setScoreSec(int score2){
+		this.scoreSec = score2;
 	}
 	
 	
-	public int saisieDplcmt(){
+	public int saisieDplcmt(int joueur){
 		
-		int direction = 0;
 		
+		
+		int direction = -2;
+		int scoreMin = 1000000000;
+		int dirChoisi = 0;
+		boolean test;
+		
+		for(direction = -2;direction<3;direction++){
+			
+			Grille grilleSec = grille;
+			int scoreSec = score;
+			
+			System.out.println("grille sec"+grilleSec + "\n grille générale"+grille);
+		
+			if(direction != 0){
+				
+				test = grilleSec.lanceurDeplacerCases(direction, false);
+				
+				
+				
+				if(scoreMin>scoreSec && test){
+					scoreMin = scoreSec;
+					dirChoisi = direction;
+				}
+			}
+		}
+		
+		grille.lanceurDeplacerCases(dirChoisi, true);
 		
 		return direction;
 	}
+	
 }
